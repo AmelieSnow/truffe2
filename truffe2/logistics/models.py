1b8d34678cf9f0133fa44201d978e5ba6309d4c2
Téo Goddet <teo.goddet@epfl.ch>
2019-11-27 11:33:55 +0100

    Revert "Merge branch 'feat/log-pdf-sync-to-gdrive'"
    This reverts commit 27a6d3e88bdea1df279d86adda42c75646ce49e4.

--- truffe2/logistics/models.py
+++ truffe2/logistics/models.py
@@ -500,12 +500,6 @@ class _SupplyReservation(GenericModel, GenericModelWithLines, GenericDelayValida
             pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("UTF-8")), result)
             result.close()
 
-	    
-	    try:
-        	os.system(u'rclone copy '+full_filename+u' GoogleDriveLog:/')
-    	    except:
-	        pass
-
     class MetaSearch(SearchableModel.MetaSearch):
 
         extra_text = u""
